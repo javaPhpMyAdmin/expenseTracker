90954f8d637431b04696a801725659792caf3021
javaPhpMyAdmin <chelobat16411@gmail.com>
2021-03-29 18:33:21 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { SpeechProvider } from '@speechly/react-client'
 
 import { Provider } from './Contex/contex'
+import App from './App';
+
 
 ReactDOM.render(
-  <Provider>
+  <SpeechProvider appId='a9046d3d-5441-4e27-8ec0-58c32e2ae2df' language='en-US'>
+    <Provider>
       <App />
-  </Provider>,
+    </Provider>
+  </SpeechProvider>,
   document.getElementById('root')
 );
 
